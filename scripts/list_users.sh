8b326ffc106c774306318716527da41082e0e52a
root <root@DESKTOP-DD03TV4.localdomain>
2025-12-17 15:41:17 +0500

    enhance auditing by listening users grouped by role

--- scripts/list_users.sh
+++ scripts/list_users.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Admins:"
+getent group admins | cut -d: -f4 | tr ',' '\n'
+
+echo
 echo "Employees:"
 getent group employees | cut -d: -f4 | tr ',' '\n'
+
+echo
+echo "Interns:"
+getent group interns | cut -d: -f4 | tr ',' '\n'
  
